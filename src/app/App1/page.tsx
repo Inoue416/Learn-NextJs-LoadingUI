f0480ce0e455e71f7de42d576273c61b9212baee
Yuya Inoue <yuyatuta@icloud.com>
2024-01-16 16:54:25 +0900

    ìnit app

---
+++ src/app/App1/page.tsx
@@ -0,0 +1,10 @@
+export const App1 = async function App1() {
+    return (
+        <>
+            <h1>Welcome App1</h1>
+            <div>
+                
+            </div>
+        </>
+    );
+} 
---
+++ src/app/App2/page.tsx
@@ -0,0 +1,10 @@
+export const App2 = async function page() {
+    return (
+        <>
+            <h1>Welcome App2</h1>
+            <div>
+                
+            </div>
+        </>
+    );
+}
